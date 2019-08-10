2e0a700d25b08af498aa5abf53aa910855c6bef4
Anoop C H <droid.anoop@gmail.com>
2019-08-10 16:01:47 +0530

    Print 4th element from first and last in a tuple

---
+++ Lab_Ex_47_Print_4th_from_first_and_last_in_Tuple.py
@@ -0,0 +1,6 @@
+# Program to Print 4th position elements from first and last in Tuple
+originalTuple = (10, "Anoop CH", 12345.5677, "God", "B Tech", 12, 12.44, 10, 1, 3, 15)
+print('Original Tuple - ', originalTuple)
+
+print('4th Element from first - ', originalTuple[3])
+print('4th Element from last - ', originalTuple[-4])
