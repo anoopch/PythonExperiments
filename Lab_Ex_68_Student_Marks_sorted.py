a7d700cf47ef93d4c4a3925bb45331b88402489c
Anoop C H <droid.anoop@gmail.com>
2019-08-11 10:18:12 +0530

    Sort based on count

--- Lab_Ex_68_Student_Marks_sorted.py
+++ Lab_Ex_68_Student_Marks_sorted.py
@@ -2,14 +2,12 @@
 noOfStudents = int(input('Enter number of students - '))
 dictionaryData = {}
 for i in range(0, noOfStudents):
-    rollNo = int(input('Enter Roll Number of Student {} - '.format(i+1)))
-    name = str(input('Enter name of Student {} - '.format(i+1)))
-    mark = float(input('Enter Mark of Student {} - '.format(i+1)))
+    rollNo = int(input('Enter Roll Number of Student {} - '.format(i + 1)))
+    name = str(input('Enter name of Student {} - '.format(i + 1)))
+    mark = float(input('Enter Mark of Student {} - '.format(i + 1)))
     dictionaryData[i] = [rollNo, name, mark]
-    print('')
 
-print('---------  Python Mark List  ---------')
-print('\tRoll No\t\tNAME\t\tMARK')
+print('\n---------  Python Mark List  ---------\n\tRoll No\t\tNAME\t\tMARK')
 for i in range(0, noOfStudents):
     currentRow = dictionaryData.get(i)
     print('\t{0}\t\t\t{1}\t\t\t{2}'.format(currentRow[0], currentRow[1], currentRow[2]))
@@ -21,15 +19,13 @@ for i in range(0, noOfStudents):
     for j in range(0, noOfStudents - i - 1):
         if dictionaryData.get(j)[2] < dictionaryData.get(j + 1)[2]:
             temp = dictionaryData[j]
-            dictionaryData[j] = dictionaryData[j+1]
-            dictionaryData[j] = temp
+            dictionaryData[j] = dictionaryData[j + 1]
+            dictionaryData[j+1] = temp
 
 # Printing with rank (Sorted)
-print('\n\n\t\t\tMark List With Rank')
-print('---------  Python Mark List  ---------')
-print('\tRoll No\t\tNAME\t\tMARK\t\tRANK')
+print('\n\n\t\t\tMark List With Rank\n---------  Python Mark List  ---------\n\tRoll No\t\tNAME\t\tMARK\t\tRANK')
 for i in range(0, noOfStudents):
     currentRow = dictionaryData.get(i)
-    print('\t{0}\t\t\t{1}\t\t\t{2}\t\t{3}'.format(currentRow[0], currentRow[1], currentRow[2], i+1))
+    print('\t{0}\t\t\t{1}\t\t\t{2}\t\t{3}'.format(currentRow[0], currentRow[1], currentRow[2], i + 1))
 
 print('--------------------------------------')
--- Lab_Ex_70_find_letter_count_sort.py
+++ Lab_Ex_70_find_letter_count_sort.py
@@ -1,8 +1,23 @@
 # Program to count each character repeating in MISSISSIPPI and sort by occurrences
-value = 'MISSISSIPPI'
+text = 'MISSISSIPPI'
+# Finding letter counts
+dictionaryData = {}
+for i in range(0, len(text)):
+    dictionaryData[text[i]] = text.count(text[i])
 
-dictionary = {}
-for i in range(0, len(value)):
-    dictionary[value[i]] = value.count(value[i])
+print('Normal Dictionary', dictionaryData)
 
-print('Non Sorted Dictionary', dictionary)
+# getting the values inside dictionary
+values = list(dictionaryData.values())
+keyValues = list(dictionaryData.keys())
+length = len(values)
+
+# Sorting
+for i in range(0, length):
+    for j in range(0, length - i - 1):
+        if int(values[j]) > int(values[j + 1]):
+            temp = dictionaryData.get(keyValues[j])
+            dictionaryData[keyValues[j]] = dictionaryData.get(keyValues[j + 1])
+            dictionaryData[keyValues[j + 1]] = temp
+
+print('Sorted Dictionary', dictionaryData)
