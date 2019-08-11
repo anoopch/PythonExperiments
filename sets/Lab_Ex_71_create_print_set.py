45efa84af67cc7c146c7b72c4582ffc2f1da028c
Anoop C H <droid.anoop@gmail.com>
2019-08-11 11:51:29 +0530

    Set - Create and iterate over a set

---
+++ sets/Lab_Ex_71_create_print_set.py
@@ -0,0 +1,13 @@
+# Program to Create a set and print it
+text = 'MISSISSIPPI'
+
+# Finding letter counts
+setData = set(text)
+print('- Set -\n', setData)
+
+setData = {1, 2, 3}
+print('- Second Set -\n', setData)
+
+# Order will be random, Each run will produce set in different order (for first only)
+# Items will be unique in set, Repeating element will be removed
+# Set has no index
---
+++ sets/Lab_Ex_72_Iterate_Over_set.py
@@ -0,0 +1,8 @@
+# Program to Iterate over a set and print it
+text = 'MISSISSIPPI'
+
+# Finding letter counts
+setData = set(text)
+
+for x in setData:
+    print(x)
