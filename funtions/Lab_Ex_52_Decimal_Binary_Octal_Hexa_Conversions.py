d427496c7cd139c1656183885f3184154b56442b
Anoop C H <droid.anoop@gmail.com>
2019-08-17 11:21:23 +0530

    number conversion among all types OCTAL-DECIMAL-HEX-BINARY

--- funtions/Lab_Ex_52_Decimal_Binary_Octal_Hexa_Conversions.py
+++ funtions/Lab_Ex_52_Decimal_Binary_Octal_Hexa_Conversions.py
@@ -3,7 +3,7 @@ def division_checker(num, div):
     return num % div == 0
 
 
-print('-------------------- MENU --------------------\n\n',
+print('-------------------- CONVERSION MENU --------------------\n\n',
 
       '\t\t1 - Decimal to Hexadecimal ',
       '\n\t\t2 - Decimal to Binary ',
@@ -21,24 +21,35 @@ print('-------------------- MENU --------------------\n\n',
       '\n\t\t11 - Octal to Hexadecimal',
       '\n\t\t12 - Octal to Binary',
 
-      '\n----------------------------------------------')
+      '\n-----------------------------------------------------')
 choice = int(input('Enter Your menu Choice - '))
-inputData = int(input('Enter the Number to convert - '))
+inputData = input('Enter the Number to convert - ')
+
 value = 0
-decimalValue = 8
 
-if choice == 2 or choice == 5 or choice == 12:
-    value = bin(inputData)
-elif choice == 4 or choice == 7 or choice == 10:
-    value = int(inputData, decimalValue)
-elif choice == 1 or choice == 8 or choice == 11:
-    value = hex(inputData)
-elif choice == 3 or choice == 6 or choice == 9:
-    value = oct(inputData)
+if choice == 1:
+    value = hex(int(inputData))
+elif choice == 2:
+    value = bin(int(inputData))
+elif choice == 3:
+    value = oct(int(inputData))
+elif choice == 4:
+    value = int(inputData, 16)
+elif choice == 5:
+    value = bin(int(inputData, 16))
+elif choice == 6:
+    value = oct(int(inputData, 16))
+elif choice == 7:
+    value = int(inputData, 2)
+elif choice == 8:
+    value = hex(int(inputData, 2))
+elif choice == 9:
+    value = oct(int(inputData, 2))
+elif choice == 10:
+    value = int(inputData, 8)
+elif choice == 11:
+    value = hex(int(inputData, 8))
+elif choice == 12:
+    value = bin(int(inputData, 8))
 
-print(value)
-
-#
-# elif choice == 2:
-# elif choice == 2:
-# elif choice == 2:
+print("Result = ", value)
