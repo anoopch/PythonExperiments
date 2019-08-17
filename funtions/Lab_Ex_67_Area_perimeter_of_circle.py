87508f4d6760bd24a19038a4759d7df004a503c8
Anoop C H <droid.anoop@gmail.com>
2019-08-17 13:16:19 +0530

    Area and Perimeter/Circumference of Circle

---
+++ funtions/Lab_Ex_67_Area_perimeter_of_circle.py
@@ -0,0 +1,7 @@
+# Program to square an entered number using Lambda/Anonymous functions
+import math
+radius = float(input('Enter the radius of circle - '))
+area = lambda value: 2 * math.pi * radius
+circumference = lambda value: math.pi * (radius ** 2)
+print("Area of circle with radius {0} is {1}".format(radius, area(radius)))
+print("Circumference of circle with radius {0} is {1}".format(radius, circumference(radius)))
