a7ded561733cf2f173abce74b7d21821db17e0bc
Anoop C H <droid.anoop@gmail.com>
2019-09-21 13:04:06 +0530

    create a table, insert data, delete table, delete database

--- databases/class_test_mysql.py
+++ databases/class_test_mysql.py
@@ -1,5 +1,44 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host='localhost', user='python', passwd='TestTestTest', auth_plugin='mysql_native_password')
+mydb = mysql.connector.connect(host='localhost', user='python', passwd='TestTestTest',
+                               auth_plugin='mysql_native_password')
+if mydb:
+    print('Connection Success')
+else:
+    print('Unable to connect')
 
-print(mydb)
+my_cursor = mydb.cursor()
+my_cursor.execute('CREATE DATABASE IF NOT EXISTS users')
+my_cursor.execute('USE users')
+my_cursor.execute('DROP TABLE IF EXISTS users')
+
+my_cursor.execute(
+    'CREATE TABLE IF NOT EXISTS employee(id int AUTO_INCREMENT PRIMARY KEY, name varchar(25), salary int)')
+# Another way
+# my_cursor.execute(
+#     'CREATE TABLE IF NOT EXISTS employee(id int AUTO_INCREMENT, name varchar(25), salary int, PRIMARY KEY(id))')
+val = [
+    ('Anoop CH', 12000),
+    ('AB', 12800),
+    ('AH', 12500),
+    ('DC CH', 13000),
+    ('E CH', 14000),
+    ('AA CH', 242424),
+    ('BB CH', 15000),
+    ('XX CH', 12000),
+    ('MD CH', 32000),
+    ('ND CH', 19000),
+    ('AD CH', 24000)
+]
+
+sql_query = "INSERT INTO employee (id, name, salary) VALUES (0, %s, %s)"
+my_cursor.executemany(sql_query, val)
+mydb.commit()
+
+my_cursor.execute('SELECT * FROM employee ORDER BY name')
+records = my_cursor.fetchall()
+if records is not None:
+    for employee in records:
+        print(employee)
+
+mydb.close()
